56f693b28261a82c76d9517dabaff735cd36c2e9
Uli Köhler <ulikoehler@online.de>
2018-05-02 20:05:39 +0200

    Fix example

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ const fs = require('mz/fs');
 const url2pdf3 = require('./index.js');
 
 url2pdf3.renderURL('https://www.google.com').then(pdf => {
-   fs.writeFile('mypdf.pdf').then(() =>
+   return fs.writeFile('mypdf.pdf', pdf).then(() =>
       console.log('See mypdf.pdf!')
    );
 });
